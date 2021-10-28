31e800d10859da272ea4152ea0a86b822d0ce813
dev2ero <dev2ero@foxmail.com>
2021-10-28 20:40:47 +0800

    update

---
+++ cpp_primer/1.9.cpp
@@ -0,0 +1,6 @@
+int main() {
+	int i = 50;
+	int ans = 0;
+	while(i != 101)
+		ans += i++;
+}
---
+++ cpp_primer/test.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+	unsigned a = 10;
+	int i = 10;
+	std::cout << i - a << std::endl;
+	return 0;
+}
