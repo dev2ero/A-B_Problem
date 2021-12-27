712fc6320e097739a559c1bdb2277a3e4bcf0dc0
dev2ero <dev2ero@foxmail.com>
2021-12-27 13:49:09 +0000

    update

--- codeforces/4A.Watermelon/solution.cpp
+++ codeforces/4A.Watermelon/solution.cpp
@@ -2,9 +2,8 @@
 
 using namespace std;
 
-
 int main() {
     int w; cin >> w;
-    cout << ((w > 2 && w % 2 == 0) ? string("YES") : string("NO")) << endl;
+    cout << (w > 2 && w % 2 == 0) ? string("YES") : string("NO") << endl;
     return 0;
 }
--- other/U144140/solution.cc
+++ other/U144140/solution.cc
@@ -3,14 +3,14 @@
 
 using namespace std;
 
-int closest(const vector<int> &v, int t);
+unsigned int sum_sub(const vector<int> &v, int i, int j, int t);
+unsigned int closest(const vector<int> &v, int t);
 
 int main() {
 	int n, q;
-	int list[n];
-	vector<int> list(n);
-
 	cin >> n >> q;
+
+	vector<int> list(n);
 	for(int i = 0; i < n; ++i) cin >> list[i];
 
 	while(q--) {
@@ -20,7 +20,17 @@ int main() {
 	}
 }
 
-int closest(const vector<int> &v, int t) {
-	// TODO
-	return 0;
+unsigned int closest(const vector<int> &v, int t) {
+	unsigned int min = UINT32_MAX;
+	for(int i = 0; i < v.size(); ++i)
+		for(int j = 0; j < v.size(); ++j)
+			sum_sub(v, i, j, t) < min ? min = sum_sub(v, i, j, t) : 0;
+	return min;
+}
+
+unsigned int sum_sub(const vector<int> &v, int i, int j, int t) {
+	int sum = 0;
+	for(int x = i; x <= j; ++x)
+		sum += v[x];
+	return abs(sum - t);
 }
