cccb7c465eca8ecb31798c76501b0e555c1d379e
dev2ero <dev2ero@foxmail.com>
2022-04-20 23:29:00 +0800

    update

---
+++ algorithm/basic/bit/basic.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <bitset>
+
+using namespace std;
+
+int main() {
+    bitset<8> b;
+    
+    b = 3;
+    cout << b << " : ";
+    cout << b.to_ulong() << " : ";
+
+    b = b << 3;
+    cout << b << " : ";
+    cout << b.to_ulong() << endl;
+
+    b  = b >> 1;
+    cout << b << " : ";
+    cout << b.to_ulong() << endl;
+    
+    b &= 0x08;
+    cout << b << " : ";
+    cout << b.to_ulong() << endl;
+    
+    b |= 0x0f;
+    cout << b << " : ";
+    cout << b.to_ulong() << endl;
+    
+    b = ~b;
+    cout << b << " : ";
+    cout << b.to_ulong() << endl;
+    
+    b ^= 0x55;
+    cout << b << " : ";
+    cout << b.to_ulong() << endl;
+}
